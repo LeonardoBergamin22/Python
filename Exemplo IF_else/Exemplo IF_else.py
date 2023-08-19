fffaf9338b953dd4d9c0354026a03730801cd56d
Leonardo Bergamin <140442294+LeonardoBergamin22@users.noreply.github.com>
2023-08-19 11:31:30 -0300

    Add files via upload

---
+++ Exemplo IF_else/Exemplo IF_else.py
@@ -0,0 +1,25 @@
+pontuacao = 0
+print("Qual aula acontece de sabado no senai")
+resposta1 = input()
+
+if resposta1 == "python":
+    pontuacao += 1
+else:
+        print("Voce errou")
+
+print("Que horas comeca")
+resposta2 = input()
+if resposta2 == "8hs":
+     pontuacao = pontuacao + 1
+else:
+    
+    print("voce errou")
+    
+print("Sua pontuacao final foi ", pontuacao)
+    
+
+        
+ 
+    
+
+
