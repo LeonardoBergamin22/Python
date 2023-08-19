9973e9b9e6de7d7aeef2426a96a1516297659a36
Leonardo Bergamin <140442294+LeonardoBergamin22@users.noreply.github.com>
2023-08-19 09:10:30 -0300

    Variaveis

---
+++ Python tipo de variaveis/Variaveis.py
@@ -0,0 +1,21 @@
+a = 10 #int
+print (type(a))
+
+b =3.2 # float
+print (type(b))
+
+c = "leonardo" #str
+print (type(c))
+
+d = True # bool
+print (type(d))
+
+l = [] #list
+print (type(l))
+
+t = ()#tuple
+print (type(t))
+
+dis = {} #dict
+print (type(dis))
+
