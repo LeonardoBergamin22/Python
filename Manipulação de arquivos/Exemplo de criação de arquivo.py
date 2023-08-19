c38c0ec81089d4188171db8160d6f56103aa941d
Leonardo Bergamin <140442294+LeonardoBergamin22@users.noreply.github.com>
2023-08-19 11:54:57 -0300

    Delete Manipulação de arquivos directory

--- Manipulação de arquivos/Exemplo de criação de arquivo.py
+++
@@ -1,9 +0,0 @@
-#Abrir o arquivo no modo escrita
-arquivo = open("exemplo.txt","w")
-
-#Escrever conteudo no arquivo
-arquivo.write("Ola, mundo!\n")
-arquivo.write("este é um exemplo de escrita de arquivo\n")
-
-#Fechar o arquivo
-arquivo.close()
